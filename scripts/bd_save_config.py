482f815d21eccd890c4076d1817e9cf832ae9e9d
Alexander Kucera <a.kucera@babylondreams.de>
2014-12-05 14:01:54 +0100

    added two scripts to save the current UI config form modo and to save the config while preserving the locked state of the config file.

---
+++ scripts/bd_save_config.py
@@ -0,0 +1,49 @@
+# python
+# coding=utf-8
+
+import os
+import traceback
+
+import lx
+
+
+__author__ = 'alex'
+
+
+# MAIN PROGRAM --------------------------------------------
+def main():
+    configpath = lx.eval("query platformservice path.path ? configname")
+    if os.path.isfile(configpath):
+        lx.out("Found Config at: " + configpath)
+        lx.out("Unlocking Config…")
+        errorcode = os.system("chflags nouchg " + configpath)
+        chmoderror = os.system("chmod 644 " + configpath)
+        if errorcode == 0 and chmoderror == 0:
+            lx.out("Saving Config…")
+            lx.eval("config.save")
+            lx.out("Locking Config…")
+            chmoderror = os.system("chmod 444 " + configpath)
+            errorcode = os.system("chflags uchg " + configpath)
+            if errorcode != 0 and chmoderror != 0:
+                lx.eval('layout.createOrClose EventLog "Event Log_layout" '
+                        'title:@macros.layouts@EventLog@ width:600 height:600 persistent:true')
+                lx.out("Locking failed with Error Codes: CHFLAGS(" + errorcode + ") and "
+                                                                                 "CHMOD(" + chmoderror + ")")
+            else:
+                lx.out("Successfully saved Config!")
+        else:
+            lx.eval('layout.createOrClose EventLog "Event Log_layout" '
+                    'title:@macros.layouts@EventLog@ width:600 height:600 persistent:true')
+            lx.out("Unlocking failed with Error Code: CHFLAGS(" + errorcode + ") and "
+                                                                              "CHMOD(" + chmoderror + ")")
+    else:
+        lx.out("Config not found")
+
+
+# END MAIN PROGRAM -----------------------------------------------
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        lx.out(traceback.format_exc())
---
+++ scripts/bd_save_form.py
@@ -0,0 +1,33 @@
+# python
+# coding=utf-8
+
+import sys
+import traceback
+
+import lx
+
+
+__author__ = 'alex'
+
+
+# MAIN PROGRAM --------------------------------------------
+def main():
+    lx.out("Saving KIT UI Config…")
+    try:
+        lx.eval("select.attr {83740604103:sheet} set")
+        lx.eval("attr.save "
+                "/Volumes/ProjectsRaid/x_Pipeline/x_AppPlugins/modo/content/Kits/BabylonDreams/configs/babylondreams_forms.cfg")
+        lx.out("Successfully saved KIT UI Config!")
+    except:
+        lx.eval('layout.createOrClose EventLog "Event Log_layout" '
+                'title:@macros.layouts@EventLog@ width:600 height:600 persistent:true')
+        lx.out("Saving KIT UI Config failed with ", sys.exc_info())
+
+
+# END MAIN PROGRAM -----------------------------------------------
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        lx.out(traceback.format_exc())
