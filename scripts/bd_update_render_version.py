589c6777fed55956e80ad9289c301c707c18d3a1
Alexander Kucera <a.kucera@babylondreams.de>
2015-01-15 10:44:21 +0100

    Updates Render Version and checks for valid paths
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

---
+++ scripts/bd_update_render_version.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Alexander Kucera
+# babylondreams.de
+
+# Description
+"""
+
+Sets up the correct render path in relation to the BabylonDreams project structure and
+automatically labels the renderOutput correctly in relation to the scene name.
+
+Also creates the necessary folder structure.
+
+Release Notes:
+
+V0.1 Initial Release
+
+"""
+
+import traceback
+import re
+import os
+
+import lx
+
+import bd_utils
+import bd_outputPattern
+
+
+# FUNCTIONS -----------------------------------------------
+
+# END FUNCTIONS -----------------------------------------------
+
+# MAIN PROGRAM --------------------------------------------
+def main():
+
+    save_selection = lx.evalN("query sceneservice selection ? all")
+
+    filepath = bd_utils.filePath()
+    if filepath is False:
+        lx.eval('layout.createOrClose EventLog "Event Log_layout" '
+                'title:@macros.layouts@EventLog@ width:600 height:600 persistent:true '
+                'open:true')
+        lx.out("ERROR Please save scene first.")
+    else:
+        filename = os.path.splitext(bd_utils.fileName())
+        fileextension = filename[1]
+
+        filename = filename[0]
+        filename = filename.rsplit("_", 1)
+        version = filename[1]
+        filename = filename[0]
+        lx.out('File "' + filename + '" is at Version ' + version + '.')
+
+        wd_regex = re.compile("/Volumes/ProjectsRaid/WorkingProjects/[^/]*/[^/]*/")
+        workingdirectory = wd_regex.match(filepath).group()
+        lx.out("Project is located at: " + workingdirectory)
+
+        renderpath = workingdirectory + "img/cg/" + filename + "/" + filename + "_" + \
+                     version + "/"
+
+        renderoutputpath = renderpath + filename + "_" + version + "_"
+        lx.out("RenderOutput will be located at: " + renderoutputpath)
+
+        renderoutputs = bd_utils.get_ids("renderOutput")
+
+        if renderoutputs:
+
+            bd_utils.makes_path(renderpath)
+
+            for x in renderoutputs:
+                lx.eval(r"select.Item %s" % x)
+                fileformat = lx.eval("item.channel renderOutput$format ?")
+                if fileformat:
+                    lx.out("Setting Render Output path to: " + renderoutputpath)
+                    lx.eval("item.channel renderOutput$filename " + renderoutputpath)
+                    lx.out("Setting Render Output format to: " + fileformat)
+                    lx.eval("item.channel renderOutput$format " + fileformat)
+
+        bd_outputPattern.main()
+
+        bd_utils.restoreSelection(save_selection)
+
+
+# END MAIN PROGRAM -----------------------------------------------
+
+if __name__ == '__main__':
+    try:
+        # Argument parsing is available through the 
+        # lx.arg and lx.args methods. lx.arg returns 
+        # the raw argument string that was passed into 
+        # the script. lx.args parses the argument string 
+        # and returns an array of arguments for easier 
+        # processing.
+
+        argsAsString = lx.arg()
+        argsAsTuple = lx.args()
+
+        main()
+
+    except:
+        lx.out(traceback.format_exc())
