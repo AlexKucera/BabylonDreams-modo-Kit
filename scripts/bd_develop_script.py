6eda504294bf51c8ab95dcf792c849f461bfdf78
Alexander Kucera <a.kucera@babylondreams.de>
2016-12-19 17:19:44 +0100

    Updated x_shading workflow
    Now is smarter and consolidated into one script.
    
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- scripts/bd_develop_script.py
+++ scripts/bd_develop_script.py
@@ -22,7 +22,7 @@ V0.1 Initial Release - 2015-05-19
 import traceback
 import modo, lx, lxu, lxifc
 import sys
-import bd_record_gl_capture  # Import the script here
+import bd_x_shading  # Import the script here
 
 
 
@@ -49,8 +49,8 @@ if __name__ == '__main__':
         argsAsString = lx.arg()
         argsAsTuple = lx.args()
 
-        reload(bd_record_gl_capture)
-        bd_record_gl_capture.main()  # call the script with any arguments here.
+        reload(bd_x_shading)
+        bd_x_shading.main()  # call the script with any arguments here.
 
     except:
         lx.out(traceback.format_exc())
---
+++ scripts/bd_x_shading.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Alexander Kucera
+# babylondreams.de
+
+# Description
+"""
+
+Release Notes:
+
+V0.1 Initial Release
+
+"""
+
+import traceback
+import bd_utils
+import lx
+import pyModo
+import modo
+
+
+# FUNCTIONS -----------------------------------------------
+
+# END FUNCTIONS -----------------------------------------------
+
+# MAIN PROGRAM --------------------------------------------
+def main():
+    shading_scenes = ["x_shading_for_animation", "x_shading_for_rendering"]
+    shading_path = "/Volumes/ProjectsRaid/WorkingProjects/peri/peri-2014_000-sharedspace/work/modo/03_shader/"
+    shading_format = ".lxo"
+
+    scene = modo.Scene()
+
+    # Get which shading style the user wants to apply
+    lx.eval("user.defNew scene_chooser integer momentary")
+    lx.eval('user.def scene_chooser username "Scene Chooser"')
+    lx.eval('user.def scene_chooser dialogname "Which shading mode do you want to use?"')
+    lx.eval("user.def scene_chooser list {0};{1}".format(shading_scenes[0], shading_scenes[1]))
+    lx.eval('user.def scene_chooser listnames "Animation;Rendering"')
+    lx.eval("user.value scene_chooser")
+
+    mode = lx.eval("user.value scene_chooser ?")
+
+    # Check if the shading scene is already present in the scene.
+    grouplocators = scene.items(itype='groupLocator', superType=True)
+    masks = scene.items(itype='mask', superType=True)
+    search = list(grouplocators + masks)
+    items = []
+
+    for item in search:
+        if "x_shading" in item.name:
+            items.append(item)
+
+    # If items is not zero we already have a shading setup in the scene and need to delete it first.
+    if len(items) != 0:
+        print "Found old shading setup. Deleting it first."
+        for item in items:
+            try:
+                safe_item = modo.Item(item.id)
+                if safe_item:
+                    lx.eval("item.delete item:{%s}" % safe_item.id)
+            except:
+                continue
+            lx.eval("!item.delete item:{0}".format(item.name))
+
+    # Now we have a clean slate and can import our new or updated shading setup.
+    lx.eval("scene.open {0}{1}{2} import".format(shading_path, mode, shading_format))
+
+    masks = scene.items(itype='mask', superType=True)
+    for mask in masks:
+        if "x_shading" in mask.name:
+            item_selection = modo.item.ItemGraph(item=mask, graphType='shadeLoc').forward()
+            modo.item.ItemGraph(item=mask, graphType='shadeLoc').disconnectInput(item_selection[0])
+            mask.name = '%s_%s' % (mask.type, mode)
+
+    # n = lx.eval('query sceneservice mask.N ? all')
+    # for i in range(n):
+    #     name = lx.eval("query sceneservice mask.name ? %s" % i)
+    #     if name == "x_shading_new (Item)":
+    #         maskid = lx.eval("query sceneservice mask.id ? %s" % i)
+    #         lx.eval("select.Item %s" % maskid)
+    #         lx.eval("mask.setMesh (all)")
+
+
+# END MAIN PROGRAM -----------------------------------------------
+
+if __name__ == '__main__':
+    try:
+        # Argument parsing is available through the 
+        # lx.arg and lx.args methods. lx.arg returns 
+        # the raw argument string that was passed into 
+        # the script. lx.args parses the argument string 
+        # and returns an array of arguments for easier 
+        # processing.
+
+        argsAsString = lx.arg()
+        argsAsTuple = lx.args()
+
+        main()
+
+    except:
+        lx.out(traceback.format_exc())
